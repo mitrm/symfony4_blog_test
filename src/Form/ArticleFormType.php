ee69bfc2c8499bd0652315b87e4ec66b058f4531
Ruslan Mitskevich <ruslantm.ru@gmail.com>
2019-10-28 12:10:36 +0500

    Валидация полей ввода

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Class ArticleFormType
@@ -20,8 +22,34 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, ['label' => false])
-            ->add('content', TextareaType::class, ['label' => false]);
+            ->add('title', TextType::class, [
+                'label' => false,
+                'required' => true,
+                'purify_html' => true,
+                'constraints' => [
+                    new Length([
+                        'max' => 100,
+                        'maxMessage' => 'Максимум {{ limit }} символов'
+                    ]),
+                    new NotBlank([
+                        'message' => 'Укажите заголовок'
+                    ])
+                ],
+                ])
+            ->add('content', TextareaType::class, [
+                'label' => false,
+                'required' => true,
+                'purify_html' => true,
+                'constraints' => [
+                    new Length([
+                        'max' => 300,
+                        'maxMessage' => 'Максимум {{ limit }} символов'
+                    ]),
+                    new NotBlank([
+                        'message' => 'Напишите текст'
+                    ])
+                ],
+            ]);
     }
 
     /**
